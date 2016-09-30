826571bbaa95b8d3c14745422da2fa1bf03ad9fc
rfripp2 <juangonzalez465@gmail.com>
2016-09-29 21:37:29 -0300

    29/09

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -24,7 +24,7 @@ $("#planets").on("click",function(){
                     for (var i in pepe.results) {
                         console.log(pepe.results[i]);
                         var resultados = pepe.results[i];
-                        clearData();
+                 
                         if(estado){
                         $("#first").html("Nombre");
                         $("#second").html("Rotation Period");
@@ -43,40 +43,5 @@ $("#planets").on("click",function(){
 
             })
 	})
-
-
-
-$("#people").on("click",function(){
-	
-		$.ajax({
-                url: "http://swapi.co/api/planets/",
-                type: 'get',
-                dataType: 'json',
-                success: function(pepe) {
-                    console.log("llego pepe: " + pepe);
-                    for (var i in pepe.results) {
-                        console.log(pepe.results[i]);
-                        var resultados = pepe.results[i];
-                        clearData();
-                        if(estado1){
-
-                        $("#first").html("Nombre");
-                        $("#second").html("Rotation Period");
-                        $("#third").html("Orbital Period");
-
-                        $('#uno').append('<div><h6>' + resultados.name + '</h6></div>')
-                        $('#dos').append('<div><h6>' + resultados.rotation_period + '</h6></div>')
-                        $('#tres').append('<div><h6>' + resultados.orbital_period + '</h6></div>')
-                    }
-                }
-                estado1 = false;
-                },
-                error: function(pepe) {
-                    console.log("no volvio nada-----");
-                }
-
-            })
-	})
-
 
 }
